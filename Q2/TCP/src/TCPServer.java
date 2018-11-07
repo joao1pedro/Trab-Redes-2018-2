4a2aae7a672ad478aba6038edef8437faa705ae7
Joao <jotape.ufc@gmail.com>
2018-11-06 21:17:52 -0300

    update

--- Q2/TCP/src/TCPServer.java
+++ Q2/TCP/src/TCPServer.java
@@ -1,4 +1,6 @@
 import java.net.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 import java.io.*;
 public class TCPServer {
@@ -27,9 +29,19 @@ class Connection extends Thread {
 	}
 	public void run(){
 		try {			                 // an echo server
+			Date date = new Date();
+			LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+			int day   = localDate.getDayOfMonth();
 			String data = in.readUTF();	                  // read a line of data from the stream
-			out.writeUTF(data +"Server date/time -- "+ new Date().toString() + " --");
-			System.out.println("Reply: Connection established");
+			//Gambiarra p/ pegar retornar uma mensagem especifica p/ cada dia do mes
+			if(day == 6) {
+				out.writeUTF(data + "Servidor -- Iam groot --" + date);
+			}
+			else if(day == 7) {
+				out.writeUTF(data + "Servidor -- Test gambiarra --" + date);
+			}
+			//out.writeUTF(data +"Server date/time -> "+ date + "O dia do mês é : " + day);
+			System.out.println("Connection succefull");
 		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
 		} catch(IOException e) {System.out.println("readline:"+e.getMessage());
 		} finally{ try {clientSocket.close();}catch (IOException e){/*close failed*/}}
